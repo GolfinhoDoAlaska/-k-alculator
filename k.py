495f4f67069392a04e3042b89e9f67cb5f5deb41
GolfinhoDoAlaska <62483097+GolfinhoDoAlaska@users.noreply.github.com>
2020-12-24 20:33:46 -0300

    Add files via upload

---
+++ k.py
@@ -0,0 +1,16 @@
+
+print("k = 1\nisso é a variavel K que vai ser usada")
+print("k-k{k.k[k.k(k-k+k)]} é o calculo a fazer")
+
+
+
+print("\n\n")
+print("resultado: ")
+
+
+k = 1
+
+k - (k * k  + (k * k + (k - k + k)))
+
+
+print(k)
